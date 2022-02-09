ef50b24b57d95bd6283614c741fcf930fcad91b1
Michael Terrell <cwithmichael@gmail.com>
2022-02-08 23:20:19 -0600

    add ability to store index on disk

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"flag"
 	"fmt"
 	"log"
@@ -13,19 +15,32 @@ var Usage string
 func init() {
 	if runtime.GOOS == "windows" {
 		Usage = `Usage:
-		akv_mem.exe FILE get KEY
-		akv_mem.exe FILE delete KEY
-		akv_mem.exe FILE insert KEY VALUE
-		akv_mem.exe FILE update KEY VALUE
+		actionkv.exe FILE get KEY
+		actionkv.exe FILE delete KEY
+		actionkv.exe FILE insert KEY VALUE
+		actionkv.exe FILE update KEY VALUE
 		`
 	} else {
 		Usage = `Usage:
-		akv_mem FILE get KEY
-		akv_mem FILE delete KEY
-		akv_mem FILE insert KEY VALUE
-		akv_mem FILE update KEY VALUE
+		actionkv FILE get KEY
+		actionkv FILE delete KEY
+		actionkv FILE insert KEY VALUE
+		actionkv FILE update KEY VALUE
 		`
 	}
+}
+
+func storeIndexOnDisk(akv *ActionKV, indexKey ByteString) error {
+	delete(akv.Index, string(indexKey))
+	b := new(bytes.Buffer)
+	e := gob.NewEncoder(b)
+	err := e.Encode(akv.Index)
+	if err != nil {
+		return err
+	}
+	akv.Index = make(map[string]uint64)
+	akv.Insert(indexKey, b.Bytes())
+	return nil
 }
 
 func main() {
@@ -54,13 +69,29 @@ func main() {
 		log.Fatalf("Unable to load data: %s\n", err.Error())
 	}
 
+	IndexKey := []byte("+index")
 	switch action {
 	case "get":
-		value, err := store.Get(ByteString(key))
+		indexAsBytes, err := store.Get(IndexKey)
 		if err != nil {
-			log.Fatalf("%s not found: %s\n", key, err.Error())
+			log.Fatal(err)
 		}
-		fmt.Printf("Value: %s\n", value)
+		b := bytes.NewBuffer(indexAsBytes)
+		d := gob.NewDecoder(b)
+		var index map[string]uint64
+		err = d.Decode(&index)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if pos, ok := index[key]; !ok {
+			log.Fatalf("Couldn't find key: %s", key)
+		} else {
+			kv, err := store.GetAt(uint64(pos))
+			if err != nil {
+				log.Fatalf("%s not found: %s\n", key, err.Error())
+			}
+			fmt.Printf("Value: %s\n", kv.Value)
+		}
 	case "delete":
 		err := store.Delete(ByteString(key))
 		if err != nil {
@@ -75,6 +106,7 @@ func main() {
 		if err != nil {
 			log.Fatalf("Failed to insert %s, %s\n", key, maybeValue)
 		}
+		storeIndexOnDisk(store, IndexKey)
 	case "update":
 		if len(values) < 4 {
 			fmt.Println(Usage)
@@ -84,6 +116,7 @@ func main() {
 		if err != nil {
 			log.Fatalf("Failed to update %s, %s\n", key, maybeValue)
 		}
+		storeIndexOnDisk(store, IndexKey)
 	default:
 		fmt.Println(Usage)
 	}
