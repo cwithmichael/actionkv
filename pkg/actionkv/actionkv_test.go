facfdf3fb5d431104f5e3ecc186e8307845d3d09
Michael Terrell <cwithmichael@gmail.com>
2022-03-13 23:01:25 -0500

    adding tests

---
+++ pkg/actionkv/actionkv_test.go
@@ -0,0 +1,37 @@
+package actionkv
+
+import (
+	"os"
+	"testing"
+)
+
+var (
+	testKey   = ByteString("3")
+	testValue = ByteString("2")
+)
+
+func TestInsertAndGet(t *testing.T) {
+	store, err := NewActionKV("test")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	defer os.Remove("test")
+	if err = store.Insert(testKey, testValue); err != nil {
+		t.Fatalf(err.Error())
+	}
+	val, err := store.Get(testKey)
+	if err != nil || val[0] != testValue[0] {
+		t.Fatalf("Wanted %s, but got %s", testValue, val)
+	}
+}
+
+func BenchmarkInsert(b *testing.B) {
+	store, err := NewActionKV("test")
+	if err != nil {
+		b.Fatal(err.Error())
+	}
+	defer os.Remove("test")
+	for n := 0; n < b.N; n++ {
+		store.Insert(testKey, testValue)
+	}
+}
